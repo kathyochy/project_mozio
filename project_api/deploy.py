fb89a9c3e97bda74fbc60cbe7753ecd9db2e20a1
kathyochy <47371664+kathyochy@users.noreply.github.com>
2019-08-06 15:19:00 -0400

    updated deploy.py

--- project_api/deploy.py
+++ project_api/deploy.py
@@ -7,9 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
+
 
 from django.core.wsgi import get_wsgi_application
+
+
+path = "/home/kathyochy/project_mozio/project_api"
+if path not in sys.path:
+	sys.path.append(path)
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_api.settings')
 
